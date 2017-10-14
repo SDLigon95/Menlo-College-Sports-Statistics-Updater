ee4a92812da5537b791b05f8c096bf1790f584bb
SDLigon95 <SDLigon@gmail.com>
2017-10-14 15:39:59 -0700

    Commit 10/14/17 Windows exe version updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=['SportsGrabber.py'],
+        options={
+        'py2exe': 
+        {
+            'includes': ['lxml.etree', 'lxml._elementpath']
+        }
+        }
+
+)
